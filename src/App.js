cc276814f3b8e94e0058046b23d8f24d918317cf
setiawanDedi05 <dssetiawan05@gmail.com>
2024-12-17 13:46:03 +0700

    dedi

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Dedi</div>;
+  return <div className="App">coba</div>;
 }
 
 export default App;
